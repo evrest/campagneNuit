0959e30dc418d440e6a8f3b539b83a6bb17866bb
evrest <hugo.marras972@gmail.com>
2023-03-09 00:47:48 +0100

    5-6 enigmes

---
+++ CaArrive/index.js
@@ -0,0 +1,8 @@
+/***************************************************************************/
+/*                                                                         */
+/*  This obfuscated code was created by Javascript Obfuscator Free Version.*/
+/*  Javascript Obfuscator Free Version can be downloaded here              */
+/*  http://javascriptobfuscator.com                                        */
+/*                                                                         */
+/***************************************************************************/
+var _$_3afb=["resize","innerWidth","target","shaking","log","p","createElement","innerHTML","SHAKE!","appendChild","body","document","Here","TCHOU TCHOUUUUUUUUUUUUUU!","addEventListener"];let counter=0;window[_$_3afb[14]](_$_3afb[0],function(_0x667C){if(_0x667C[_$_3afb[2]][_$_3afb[1]]< 768){console[_$_3afb[4]](_$_3afb[3]);var _0x6696=document[_$_3afb[6]](_$_3afb[5]);_0x6696[_$_3afb[7]]= _$_3afb[8];this[_$_3afb[11]][_$_3afb[10]][_$_3afb[9]](_0x6696);counter+= 1};if(counter== 100){console[_$_3afb[4]](_$_3afb[12]);var _0x6696=document[_$_3afb[6]](_$_3afb[5]);_0x6696[_$_3afb[7]]= _$_3afb[13];this[_$_3afb[11]][_$_3afb[10]][_$_3afb[9]](_0x6696)}},true)
